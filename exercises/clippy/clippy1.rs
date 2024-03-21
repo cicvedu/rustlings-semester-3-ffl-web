925929a95341eec6c47ff7428cba249e0aa93ef0
schnk <3021593440@qq.com>
2024-03-21 10:11:04 +0800

    end

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -11,16 +11,27 @@
 
 
 
-use std::f32;
+// use std::f32;
 
+// fn main() {
+//     let pi = 3.14f32;
+//     let radius = 5.00f32; 
+
+//     let area = pi * f32::powf(radius, 2.0);
+
+//     println!(
+//         "The area of a circle with radius {:.2} is {:.5}!",
+//         radius, area
+//     )
+// }
 fn main() {
-    let pi = 3.14f32;
-    let radius = 5.00f32; 
+    let pi = std::f32::consts::PI;
+    let radius = 5.00;
 
-    let area = pi * f32::powf(radius, 2.0);
+    let area = pi * radius * radius;
 
     println!(
         "The area of a circle with radius {:.2} is {:.5}!",
         radius, area
-    )
+    );
 }
